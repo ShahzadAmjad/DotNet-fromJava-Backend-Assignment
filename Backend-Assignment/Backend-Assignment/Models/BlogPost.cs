5e9aa2e0539ea5f7a7a5712a7c7c7ae54ed1d68b
Shehzad Amjad <samjad@arvensysgroup.com>
2024-08-31 00:28:56 +0500

    add key attribute in model class

--- Backend-Assignment/Backend-Assignment/Models/BlogPost.cs
+++ Backend-Assignment/Backend-Assignment/Models/BlogPost.cs
@@ -1,11 +1,14 @@
-﻿namespace Backend_Assignment.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend_Assignment.Models
 {
     public class BlogPost
     {
+        [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string Author { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Author { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
